9a31ece739f2279a75a80ed0a83b43792811e544
bala-amypo <bala@amypo.in>
2025-12-15 10:43:00 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String sku;
+    private String name;
+    private String category;
+    private BigDecimal 
+}
