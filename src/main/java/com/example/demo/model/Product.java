78f8f6a173e06d4323c6947081800d469cd546d1
bala-amypo <bala@amypo.in>
2025-12-16 08:41:21 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -12,5 +12,8 @@ public class Product {
     private Boolean active;
     private Timestamp createdAt;
     public Product(){}
-    
+    public Product(String sku,String name,String category,BigDecimal price,Boolean active,Timestamp createdAt){
+        this.sku=sku;
+        this.name=name;
+    }
 }
