f0cec1f4862f7c9245c2070b5e8fbf9d444f3100
bala-amypo <bala@amypo.in>
2025-12-18 07:39:38 +0000

    User commit

--- src/main/java/com/example/demo/model/BundleRule.java
+++ src/main/java/com/example/demo/model/BundleRule.java
@@ -1,49 +1,59 @@
-// package com.example.E_commerce.model;
+package com.example.demo.model;
 
-// public class BundleRule {
-//     private Long id;
-//     private String ruleName;
-//     private String requiredProductIds;
-//     private Double discountPercentage;
-//     private Boolean active;
-//     public BundleRule(){}
-//     public BundleRule(String ruleName, String requiredProductIds, Double discountPercentage, Boolean active) {
-//         this.ruleName = ruleName;
-//         this.requiredProductIds = requiredProductIds;
-//         this.discountPercentage = discountPercentage;
-//         this.active = active;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public void setRuleName(String ruleName) {
-//         this.ruleName = ruleName;
-//     }
-//     public void setRequiredProductIds(String requiredProductIds) {
-//         this.requiredProductIds = requiredProductIds;
-//     }
-//     public void setDiscountPercentage(Double discountPercentage) {
-//         this.discountPercentage = discountPercentage;
-//     }
-//     public void setActive(Boolean active) {
-//         this.active = active;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public String getRuleName() {
-//         return ruleName;
-//     }
-//     public String getRequiredProductIds() {
-//         return requiredProductIds;
-//     }
-//     public Double getDiscountPercentage() {
-//         return discountPercentage;
-//     }
-//     public Boolean getActive() {
-//         return active;
-//     }
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class BundleRule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique=true)
+    private String ruleName;
+    private String requiredProductIds;
+    private Double discountPercentage;
+    private Boolean active=true;
+    public BundleRule(){}
+    public BundleRule(String ruleName, String requiredProductIds, Double discountPercentage, Boolean active) {
+        this.ruleName = ruleName;
+        this.requiredProductIds = requiredProductIds;
+        this.discountPercentage = discountPercentage;
+        this.active = active;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+    public void setRequiredProductIds(String requiredProductIds) {
+        this.requiredProductIds = requiredProductIds;
+    }
+    public void setDiscountPercentage(Double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getRuleName() {
+        return ruleName;
+    }
+    public String getRequiredProductIds() {
+        return requiredProductIds;
+    }
+    public Double getDiscountPercentage() {
+        return discountPercentage;
+    }
+    public Boolean getActive() {
+        return active;
+    }
     
 
     
-// }
+}
---
+++ src/main/java/com/example/demo/repository/BundleRuleRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.BundleRule;
+
+public interface BundleRuleRepository extends JpaRepository<BundleRule,Long>{
+
+    List<BundleRule> findByActive(boolean b);
+    
+}
