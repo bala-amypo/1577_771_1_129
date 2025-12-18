a179a4a7caae9d04b0dbf964ef3c0de9b8698391
bala-amypo <bala@amypo.in>
2025-12-18 09:43:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -1,58 +1,60 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
 
-// import com.example.demo.model.Product;
-// import com.example.demo.repository.ProductRepository;
-// import com.example.demo.service.ProductService;
-// @Service
-// public class ProductServiceImpl implements ProductService {
-//     @Autowired
-//     ProductRepository productRepository;
-//     @Override
-//     public Product createProduct(Product product){
-//         System.out.println("In Impl "+product.getName());
-//         return productRepository.save(product);
-//     }
-// //     @Override
-// //     public Product updateProduct(Long id,Product product){
-// //         Optional<Product> lis=productRepository.findById(id);
-// //         if(lis.isPresent()){
-// //             Product old=lis.get();
-// //             old.setSku(product.getSku());
-// //             old.setName(product.getName());
-// //             old.setCategory(product.getCategory());
-// //             old.setPrice(product.getPrice());
-// //             old.setActive(product.getActive());
-// //             old.setCreatedAt(product.getCreatedAt());
-// //             return productRepository.save(old);
+@Service
+public class ProductServiceImpl implements ProductService {
 
-// //     }
-// //     return null;
-// // }
-//     @Override
-//     public Product getProductById(Long id){
-//        Optional<Product> lis = productRepository.findById(id);
-//       return lis.orElse(null);
-        
-//     }
-//     @Override
-//     public List<Product> getAllProducts(){
-//         return productRepository.findAll();
-//     }
-//     // @Override
-//     // public void deactiveProduct(Long id){
-//     //     Product product=getProductById(id);
-//     //     product.setActive(false);
-//     //     productRepository.save(product);
-//     // }
+    @Autowired
+    ProductRepository productRepository;
 
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
 
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        Optional<Product> optional = productRepository.findById(id);
 
+        if (optional.isPresent()) {
+            Product old = optional.get();
+            old.setSku(product.getSku());
+            old.setName(product.getName());
+            old.setCategory(product.getCategory());
+            old.setPrice(product.getPrice());
+            old.setActive(product.getActive());
+            return productRepository.save(old);
+        }
+        return null;
+    }
 
-// }
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+    
+    @Override
+    public void deactiveProduct(Long id) {
+        Optional<Product> optional = productRepository.findById(id);
+
+        if (optional.isPresent()) {
+            Product product = optional.get();
+            product.setActive(false);
+            productRepository.save(product);
+        }
+    }
+}
