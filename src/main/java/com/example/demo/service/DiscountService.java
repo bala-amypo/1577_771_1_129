964fb03d56670834cb2ce8cc458f6d33966ae72f
bala-amypo <bala@amypo.in>
2025-12-18 16:21:52 +0000

    User commit

--- src/main/java/com/example/demo/service/DiscountService.java
+++ src/main/java/com/example/demo/service/DiscountService.java
@@ -2,7 +2,7 @@ package com.example.E_commerce.service;
 
 import java.util.List;
 
-import com.example.E_commerce.model.DiscountApplication;
+import com.example.demo.model.DiscountApplication;
 
 public interface DiscountService {
     void evaluateDiscounts(Long cartId);
