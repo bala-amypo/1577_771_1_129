5d87ba207db911cd9409417d3ac6dd49c8ec0655
bala-amypo <bala@amypo.in>
2025-12-18 14:30:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+public class AuthResponse {
+    private String message;
+    public AuthResponse(String message){
+        this.message=message;
+    }
+    public AuthResponse(){}
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+    
+}
