b74b97f12fe90ddfaf7713f4c536e5dcccf2e252
bala-amypo <bala@amypo.in>
2025-12-18 14:50:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CartItemService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.CartItem;
+
+public interface CartItemService {
+    CartItem addItem(Long cartId,Long productId,Integer quantity);
+    CartItem updateItem(Long id,Integer quantity);
+    List<CartItem> getItemsForCart(Long cartId);
+    void removeItem(Long id);
+}
