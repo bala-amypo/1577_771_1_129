269e2485c23f7618a52d854edc62ed31113140c6
bala-amypo <bala@amypo.in>
2025-12-18 14:31:14 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,43 +1,53 @@
-// package com.example.E_commerce.model;
+package com.example.demo.model;
 
-// public class User {
-//     private long id;
-//     private String email;
-//     private String password;
-//     private String role;
-//     public User(){
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-//     }
-//     public User(String email, String password, String role) {
-//         this.email = email;
-//         this.password = password;
-//         this.role = role;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
-//     public void setRole(String role) {
-//         this.role = role;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public String getEmail() {
-//         return email;
-//     }
-//     public String getPassword() {
-//         return password;
-//     }
-//     public String getRole() {
-//         return role;
-//     }
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false,unique = true)
+    private String email;
+    private String password;
+    private String role;
+    public User(){
+
+    }
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
 
 
     
-// }
+}
