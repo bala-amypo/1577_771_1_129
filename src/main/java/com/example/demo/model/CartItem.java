a013c30e8f64d059d78b91b3645fe575f9c17dc5
bala-amypo <bala@amypo.in>
2025-12-18 14:46:57 +0000

    User commit

--- src/main/java/com/example/demo/model/CartItem.java
+++ src/main/java/com/example/demo/model/CartItem.java
@@ -1,41 +1,57 @@
-// package com.example.E_commerce.model;
+package com.example.demo.model;
 
-// public class CartItem {
-//     private Long id;
-//     private Cart cart;
-//     private Product product;
-//     private int quantity;
-//     public CartItem(){}
-//     public CartItem(Cart cart, Product product, int quantity) {
-//         this.cart = cart;
-//         this.product = product;
-//         this.quantity = quantity;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public void setCart(Cart cart) {
-//         this.cart = cart;
-//     }
-//     public void setProduct(Product product) {
-//         this.product = product;
-//     }
-//     public void setQuantity(int quantity) {
-//         this.quantity = quantity;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public Cart getCart() {
-//         return cart;
-//     }
-//     public Product getProduct() {
-//         return product;
-//     }
-//     public int getQuantity() {
-//         return quantity;
-//     }
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Positive;
+@Entity
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="cart_id")
+    private Cart cart;
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+    @Positive(message="Quantity must be positive")
+    private int quantity;
+    public CartItem(){}
+    public CartItem(Cart cart, Product product, int quantity) {
+        this.cart = cart;
+        this.product = product;
+        this.quantity = quantity;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public Long getId() {
+        return id;
+    }
+    public Cart getCart() {
+        return cart;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
 
 
     
-// }
+}
