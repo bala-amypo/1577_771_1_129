bb13be1d9893387c0099cfc5eabfbd117f761577
bala-amypo <bala@amypo.in>
2025-12-18 15:01:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/DiscountController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.DiscountApplication;
+import com.example.demoservice.DiscountService;
+
+@RestController
+@RequestMapping("/api/discounts")
+public class DiscountController {
+
+    @Autowired
+    DiscountService discountService;
+
+    @PostMapping("/evaluate/{cartId}")
+    public String evaluate(@PathVariable Long cartId) {
+        discountService.evaluateDiscounts(cartId);
+        return "Discount evaluated";
+    }
+
+    @GetMapping("/{id}")
+    public DiscountApplication getById(@PathVariable Long id) {
+        return discountService.getApplicationById(id);
+    }
+
+    @GetMapping("/cart/{cartId}")
+    public List<DiscountApplication> getByCart(@PathVariable Long cartId) {
+        return discountService.getApplicationsForCart(cartId);
+    }
+}
