71c0ca38b4314f1ebb2f76ea4390a2bf66b587fa
bala-amypo <bala@amypo.in>
2025-12-19 08:38:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,46 +1,46 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.example.demo.model.Cart;
-// import com.example.demo.service.CartService;
+import com.example.demo.model.Cart;
+import com.example.demo.service.CartService;
 
-// import jakarta.validation.Valid;
+import jakarta.validation.Valid;
 
-// @RestController
-// @RequestMapping("/api/carts")
-// public class CartController {
-//     @Autowired
-//     CartService cartService;
-//     @PostMapping("/{userId}")
-//     public Cart create(@Valid @RequestBody Long userId){ 
-//         return cartService.createCart(userId);
-//     }
+@RestController
+@RequestMapping("/api/carts")
+public class CartController {
+    @Autowired
+    CartService cartService;
+    @PostMapping("/{userId}")
+    public Cart create(@Valid @RequestBody Long userId){ 
+        return cartService.createCart(userId);
+    }
  
-//     @GetMapping("/{id}")
-//     public Cart get(@PathVariable Long id){
-//         return cartService.getCartById(id);
-//     }
-//     @GetMapping("/user/{userId}")
-//     public Cart getbyid(@PathVariable Long userId){
-//         return cartService.getCartByUserId(userId);
-//     }
-//     @PutMapping("/{id}/deactivate")
-//     public ResponseEntity<String> put(@PathVariable Long id){
-//          cartService.deactivateCart(id);
-//          return ResponseEntity.ok("Product deactivated");
-//     }
+    @GetMapping("/{id}")
+    public Cart get(@PathVariable Long id){
+        return cartService.getCartById(id);
+    }
+    @GetMapping("/user/{userId}")
+    public Cart getbyid(@PathVariable Long userId){
+        return cartService.getCartByUserId(userId);
+    }
+    @PutMapping("/{id}/deactivate")
+    public ResponseEntity<String> put(@PathVariable Long id){
+         cartService.deactivateCart(id);
+         return ResponseEntity.ok("Product deactivated");
+    }
 
 
-// }
+}
 
 
--- src/main/java/com/example/demo/model/Cart.java
+++ src/main/java/com/example/demo/model/Cart.java
@@ -1,67 +1,67 @@
-// package com.example.demo.model;
-// import java.util.*;
+package com.example.demo.model;
+import java.util.*;
 // import com.example.demo.model.CartItem;
 // import com.example.demo.model.BundleRule;
-// import java.sql.Timestamp;
-// import jakarta.persistence.OneToMany;
+import java.sql.Timestamp;
+import jakarta.persistence.OneToMany;
 
-// import jakarta.persistence.*;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// @Entity
-// public class Cart {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     private Long userId;
-//     private Timestamp createdAt;
-//     private Timestamp updatedAt;
-//     private Boolean active=true;
-//     @OneToMany(mappedBy="cart")
-//     public List<CartItem> lis1=new ArrayList<>();
-//     @OneToMany(mappedBy="cart")
-//     public List<DiscountApplication>lis2=new ArrayList<>();
-//     public Cart(){}
-//     public Cart(Long userId, Timestamp createdAt, Timestamp updatedAt,Boolean active) {
-//         this.userId = userId;
-//         this.createdAt = createdAt;
-//         this.updatedAt = updatedAt;
-//         this.active=active;
-//     }
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
-//     public void setUserId(Long userId) {
-//         this.userId = userId;
-//     }
-//     public void setCreatedAt(Timestamp createdAt) {
-//         this.createdAt = createdAt;
-//     }
-//     public void setUpdatedAt(Timestamp updatedAt) {
-//         this.updatedAt = updatedAt;
-//     }
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private Boolean active=true;
+    // @OneToMany(mappedBy="cart")
+    // public List<CartItem> lis1=new ArrayList<>();
+    // @OneToMany(mappedBy="cart")
+    // public List<DiscountApplication>lis2=new ArrayList<>();
+    public Cart(){}
+    public Cart(Long userId, Timestamp createdAt, Timestamp updatedAt,Boolean active) {
+        this.userId = userId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.active=active;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
     
-//     public void setActive(Boolean active) {
-//         this.active = active;
-//     }
-//     public Long getId() {
-//         return id;
-//     }
-//     public Long getUserId() {
-//         return userId;
-//     }
-//     public Timestamp getCreatedAt() {
-//         return createdAt;
-//     }
-//     public Timestamp getUpdatedAt() {
-//         return updatedAt;
-//     }
-//     public Boolean getActive() {
-//         return active;
-//     }
-
-    
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+    public Long getId() {
+        return id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+    public Boolean getActive() {
+        return active;
+    }
 
     
-// }
+
+    
+}
