f9d0dae20a531408763379c5a66bdbdb5cebfb36
bala-amypo <bala@amypo.in>
2025-12-20 16:52:56 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,34 +1,34 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.dto.AuthRequest;
-// import com.example.demo.dto.AuthResponse;
-// import com.example.demo.dto.RegisterRequest;
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.service.AuthService;
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.AuthService;
 
-// @Service
-// public class AuthServiceImpl implements AuthService {
+@Service
+public class AuthServiceImpl implements AuthService {
 
-//     @Autowired
-//     UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-//     @Override
-//     public AuthResponse register(RegisterRequest request) {
-//         User user = new User();
-//         user.setEmail(request.getEmail());
-//         user.setPassword(request.getPassword());
-//         user.setRole("USER");
-//         userRepository.save(user);
+    @Override
+    public AuthResponse register(RegisterRequest request) {
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRole("USER");
+        userRepository.save(user);
 
-//         return new AuthResponse("User registered successfully");
-//     }
+        return new AuthResponse("User registered successfully");
+    }
 
-//     @Override
-//     public AuthResponse login(AuthRequest request) {
-//         return new AuthResponse("Login successful");
-//     }
-// }
+    @Override
+    public AuthResponse login(AuthRequest request) {
+        return new AuthResponse("Login successful");
+    }
+}
