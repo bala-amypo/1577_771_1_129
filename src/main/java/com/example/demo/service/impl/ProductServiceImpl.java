d75dc3123a1e855254d48ed3b83fc8a7e934a19c
bala-amypo <bala@amypo.in>
2025-12-21 07:52:29 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.demo.service.impl;
 import com.example.demo.model.Product;
 import com.example.demo.repository.ProductRepository;
 import com.example.demo.service.ProductService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service   // ⭐ REQUIRED
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository repository;
@@ -14,34 +16,44 @@ public class ProductServiceImpl implements ProductService {
         this.repository = repository;
     }
 
+    @Override
     public Product createProduct(Product product) {
+
         repository.findBySku(product.getSku())
                 .ifPresent(p -> {
                     throw new IllegalArgumentException("SKU already exists");
                 });
+
         return repository.save(product);
     }
 
+    @Override
     public Product updateProduct(Long id, Product product) {
+
         Product existing = getProductById(id);
         existing.setName(product.getName());
         existing.setCategory(product.getCategory());
         existing.setPrice(product.getPrice());
+
         return repository.save(existing);
     }
 
+    @Override
     public Product getProductById(Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found"));
     }
 
+    @Override
     public List<Product> getAllProducts() {
         return repository.findAll();
     }
 
+    @Override
     public void deactivateProduct(Long id) {
-        Product p = getProductById(id);
-        p.setActive(false);
-        repository.save(p);
+
+        Product product = getProductById(id);
+        product.setActive(false);
+        repository.save(product);
     }
 }
