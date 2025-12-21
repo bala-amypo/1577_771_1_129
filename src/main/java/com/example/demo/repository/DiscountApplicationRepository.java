3983c4185ba9c54a57c0b62c03de7d75332b45ad
bala-amypo <bala@amypo.in>
2025-12-21 11:53:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/DiscountApplicationRepository.java
+++ src/main/java/com/example/demo/repository/DiscountApplicationRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.DiscountApplication;
+
 import java.util.List;
 
 public interface DiscountApplicationRepository
         extends JpaRepository<DiscountApplication, Long> {
 
-    List<DiscountApplication> findByCartId(Long cartId);
+    List<DiscountApplication> findByCart_Id(Long cartId);
 }
