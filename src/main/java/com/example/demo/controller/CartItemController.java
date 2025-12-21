88f7d89d3ac12ad4654ffcb0473d3bdadf88683a
bala-amypo <bala@amypo.in>
2025-12-21 07:55:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/CartItemController.java
+++ src/main/java/com/example/demo/controller/CartItemController.java
@@ -1,41 +1,41 @@
-// package com.example.demo.controller;
+package com.example.demo.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-// import com.example.demo.model.CartItem;
-// import com.example.demo.service.CartItemService;
+import com.example.demo.model.CartItem;
+import com.example.demo.service.CartItemService;
 
-// @RestController
-// @RequestMapping("/api/cart-items")
-// public class CartItemController {
+@RestController
+@RequestMapping("/api/cart-items")
+public class CartItemController {
 
-//     @Autowired
-//     CartItemService cartItemService;
+    @Autowired
+    CartItemService cartItemService;
 
-//     @PostMapping
-//     public CartItem add(@RequestParam Long cartId,@RequestParam Long productId,@RequestParam Integer quantity) {
-//         return cartItemService.addItem(cartId, productId, quantity);
-//     }
+    @PostMapping
+    public CartItem add(@RequestParam Long cartId,@RequestParam Long productId,@RequestParam Integer quantity) {
+        return cartItemService.addItem(cartId, productId, quantity);
+    }
 
-//     @PutMapping("/{id}")
-//     public String update(@PathVariable Long id, Integer quantity) {
-//         if (cartItemService.updateItem(id, quantity) != null) {
-//             return "Successful";
-//         }
-//         return "Not successful";
-//     }
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, Integer quantity) {
+        if (cartItemService.updateItem(id, quantity) != null) {
+            return "Successful";
+        }
+        return "Not successful";
+    }
 
-//     @GetMapping("/cart/{cartId}")
-//     public List<CartItem> getItems(@PathVariable Long cartId) {
-//         return cartItemService.getItemsForCart(cartId);
-//     }
+    @GetMapping("/cart/{cartId}")
+    public List<CartItem> getItems(@PathVariable Long cartId) {
+        return cartItemService.getItemsForCart(cartId);
+    }
 
-//     @DeleteMapping("/{id}")
-//     public String delete(@PathVariable Long id) {
-//         cartItemService.removeItem(id);
-//         return "Item removed";
-//     }
-// }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id) {
+        cartItemService.removeItem(id);
+        return "Item removed";
+    }
+}
