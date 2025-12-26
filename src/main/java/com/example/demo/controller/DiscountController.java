b9e8e1e1163496fa3bb373ac3b72527e6d49ef70
bala-amypo <bala@amypo.in>
2025-12-26 17:07:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/DiscountController.java
+++ src/main/java/com/example/demo/controller/DiscountController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import com.example.demo.model.DiscountApplication;
 import com.example.demo.service.DiscountService;
-
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,10 +27,10 @@ public class DiscountController {
     public DiscountApplication getById(@PathVariable Long id) {
         return discountService.getApplicationById(id);
     }
-    @PostMapping("/evaluate/{cartId}")
-public String evaluate(@PathVariable Long cartId) {
-    discountService.evaluateDiscounts(cartId);
-    return "Discounts evaluated successfully for cartId: " + cartId;
-}
 
+    @PostMapping("/evaluate/{cartId}")
+    public String evaluate(@PathVariable Long cartId) {
+        discountService.evaluateDiscounts(cartId);
+        return "Discounts evaluated successfully for cartId: " + cartId;
+    }
 }
